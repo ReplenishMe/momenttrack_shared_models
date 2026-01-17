846d72453f351858759ec4d202c4c74b3137f95a
curiouspaul1 <asalupaul36@gmail.com>
2026-01-17 01:27:14 +0100

    updates

--- momenttrack_shared_models/core/database/models/license_plate_move.py
+++ momenttrack_shared_models/core/database/models/license_plate_move.py
@@ -57,32 +57,22 @@ class LicensePlateMove(db.BaseModel, IdMixin, TimestampMixin, BelongsToOrgMixin)
         "LicensePlate", backref="license_plate_move", lazy="joined"
     )
 
-    def update_associated_report(self, last_interaction_time):
-        from momenttrack_shared_models.core.schemas import \
-              LicensePlateReportSchema
-        lp_id = self.license_plate.id
-        user: User = db.session.scalar(
+    def update_associated_report(self, last_interaction_time, session):
+        from core.database.models import EverythingReport
+        lp_id = self.license_plate.lp_id
+        user: User = session.scalar(
             select(User).where(User.id == self.user_id)
-        )
-        # refresh lp fields
-        lp_payload = LicensePlateReportSchema().dump(
-            self.license_plate
         )
         payload = {
             "who_moved_last": user.person_id,
             "when_last_movement": self.created_at,
             "last_interaction": last_interaction_time,
         }
-        data = {**lp_payload, **payload}
-        print(data)
-        resp = open_client.update(
-            index="everything_report_idx",
-            id=lp_id,
-            body={'doc': data}
-        )
-        shards = resp.get("_shards", {})
-        if shards.get("failed", 0) > 0:
-            raise Exception(
-                f"Warning: Operation succeeded, "
-                f"but {shards['failed']} shards failed."
+
+        report = session.scalar(
+            select(EverythingReport).where(
+                EverythingReport.lp_id == lp_id
             )
+        )
+        for k, v in payload.items():
+            setattr(report, k, v)
